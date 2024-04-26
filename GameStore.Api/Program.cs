93f32ca37cd236efdc966e9df8961977e9088a50
Ender Şık <mailtoender@gmail.com>
2024-04-26 23:22:19 +0300

    Data Model definitions for mapping the app to relational database

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,7 +1,12 @@
+using GameStore.Api.Data;
 using GameStore.Api.Dtos;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 app.MapGamesEndPoints();
